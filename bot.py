227d7a670de59e170d74e6c0ec88a58ae07c18a6
Emil-F <e.fagerjord@gmail.com>
2022-06-20 01:24:37 +0200

    Reading top 5 posts from subreddit.

--- bot.py
+++ bot.py
@@ -10,5 +10,11 @@ reddit = praw.Reddit(
     password=config.password,
 )
 
+subreddit = reddit.subreddit("learnpython")
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
 
 print(reddit.user.me()) #Print  username
